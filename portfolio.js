b195a1def43e5675b0c8e009feddd99e1c6eda6e
AnaSeiculescu <anaseiculescu@gmail.com>
2023-07-18 15:43:37 +0300

    changing buttons in circle buttons and linking each one of them to a portfolio project

--- portfolio.js
+++ portfolio.js
@@ -2,13 +2,16 @@
 const container = document.querySelector("#center-container");
 const RADIUS = 250; // adjust to screen size
 
+let project_links = ["https://anaseiculescu.github.io/about-me/", "https://anaseiculescu.github.io/breakfast-recipes/", "https://anaseiculescu.github.io/ana_s_color_picker/", "https://anaseiculescu.github.io/collection-map/", "https://anaseiculescu.github.io/to-do-list/"];
+let project_names = ["about me", "breakfast recipes", "color picker", "collection map", "to do list"];
+
 function getPointOnCircle(angleDegrees) {
     //Convert angle to radians
     const angleRadians = (angleDegrees * Math.PI) / 180;
 
     // Calculate x and y coordinates
-    const x = RADIUS * Math.cos(angleRadians);
-    const y = RADIUS * Math.sin(angleRadians);
+    const x = RADIUS * Math.sin(angleRadians);
+    const y = -1 * RADIUS * Math.cos(angleRadians);
 
     // Return coordinates as an object
     return { x, y };
@@ -26,19 +29,19 @@ function getPointsOnCircle(numPoints) {
 }
 
 function createButtonsOnCircle(pointsOnCircle) {
-    const buttons = pointsOnCircle.map(({ x, y }, i) => {
+    const buttons = pointsOnCircle.forEach(({ x, y }, index) => {
         const btn = document.createElement("button");
-        btn.textContent = `Button ${i +1}`;
+        btn.textContent = project_names[index];
         btn.style.transform = `translate(${x}px, ${y}px) translate(-50%, -50%)`;
         container.appendChild(btn);
 
-        return btn;
+        btn.addEventListener("click", function() {
+            window.open(project_links[index]);
+        });
     });
-
-    return buttons;
 }
 
-const pointsOnCircle = getPointsOnCircle(6);
+const pointsOnCircle = getPointsOnCircle(5);
 
 createButtonsOnCircle(pointsOnCircle);
 
